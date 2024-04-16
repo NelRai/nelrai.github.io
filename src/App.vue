eb67a4d8528ae5a9b2fbd777258bea9d625edd0f
Nelson Rainsborough <nelson.rainsborough@gmail.com>
2024-04-16 12:04:13 +0200

    First Components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 import Navigation from './components/Navigation.vue'
+import Content from './components/Content.vue'
+import Clipboard from './components/Clipboard.vue'
+import Projects from './components/Projects.vue'
 
 
 </script>
@@ -10,24 +12,17 @@ import Navigation from './components/Navigation.vue'
 
 
 
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-
+<main>
   <Header />
 
   <Navigation />
+  <Clipboard />
+  <Content />
+  <Projects />
 
 
 
-  <HelloWorld msg="Vite + Vue" />
-
-
+</main>
 
 
 </template>
