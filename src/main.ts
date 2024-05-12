61db730ccfbb6fa1d2aa65b25eb295195073f243
Nelson Rainsborough <nelson.rainsborough@gmail.com>
2024-05-12 21:54:01 +0200

    PrimeVue added

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+//@ts-ignore
+import Lara from './presets/lara';  //import preset
+
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    unstyled: true,
+    pt: Lara                            //apply preset        
+});
+app.mount('#app');
+// createApp(App).mount('#app')
